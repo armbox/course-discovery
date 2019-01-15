46f781ad2d1a8d0adb2d67c32d9463cd712983a7
Quho <armbox@gmail.com>
2019-01-15 16:46:53 +0900

    쓰로틀 값 늘림..

--- course_discovery/settings/base.py
+++ course_discovery/settings/base.py
@@ -361,7 +361,7 @@ REST_FRAMEWORK = {
         'course_discovery.apps.core.throttles.OverridableUserRateThrottle',
     ),
     'DEFAULT_THROTTLE_RATES': {
-        'user': '100/hour',
+        'user': '10000/hour',
     },
 }
 
