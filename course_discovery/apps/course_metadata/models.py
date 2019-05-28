a01d9b588a417485735fb4843997136cf9bca889
theun0524 <theun@postech.ac.kr>
2019-05-28 18:24:35 +0900

    Person.bio 이전 코드 복구 예정.

--- course_discovery/apps/course_metadata/models.py
+++ course_discovery/apps/course_metadata/models.py
@@ -286,6 +286,7 @@ class Person(TimeStampedModel):
     salutation = models.CharField(max_length=10, null=True, blank=True)
     given_name = models.CharField(max_length=255)
     family_name = models.CharField(max_length=255, null=True, blank=True)
+    # bio = models.TextField(null=True, blank=True)
     bio = FroalaField(null=True, blank=True)
     bio_language = models.ForeignKey(LanguageTag, null=True, blank=True)
     profile_image_url = models.URLField(null=True, blank=True)
